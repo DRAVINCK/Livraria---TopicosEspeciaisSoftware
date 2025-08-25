a70a3ca1701d539b899916f59bbb040af9fb4bd8
Lucas Teixeira <teixeirajob.one@gmail.com>
2025-08-24 21:50:09 -0300

    criado constantes, exemplo de entidade, lista e metodos do menu e cadastro

---
+++ livraria.py
@@ -0,0 +1,54 @@
+# Constante
+DESCONTO_PADRAO = 0.0117
+
+#Entidade
+"""
+item = {nome, preco, quantidade}
+"""
+
+#Listas
+itens = []
+
+#metodos
+
+#fazer metodo de exibir o menu
+def exibir_menu():
+    print("\n--- Menu da Papelaria ---")
+    print("1. Cadastrar novo item")
+    print("2. Listar todos os itens")
+    print("3. Realizar venda")
+    print("4. Sair")
+    return input("Escolha uma opção: ")
+
+
+# fazer o metodo para cadastrar item
+def cadastrar_item(itens):
+    print("\n--- Cadastro de Novo Item ---")
+    nome = input("Digite o nome do item (ex: Caneta Azul): ").replace(",", ".").replace(" ", "")
+
+    try:
+        preco = float(input("Digite o preço: "))
+        quantidade = int(input("Digite a quantidade em estoque: ").replace(" ", ""))
+    except ValueError:
+        print("\nErro: Preço e quantidade precisam ser números. Operação cancelada.")
+        return
+
+    novo_item = {
+        "nome": nome,
+        "preco": preco,
+        "quantidade": quantidade
+    }
+    itens.append(novo_item)
+    print(f"\nItem '{nome}' cadastrado com sucesso!")
+
+# fazer o metodo para listar os itens
+
+
+#fazer metodo de realizar venda
+
+
+# define a logica de main para navegar entre os metodos -- fazer por ultimo
+
+
+"""if __name__ == "__main__":
+    main()""""
